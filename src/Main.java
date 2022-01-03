0e528079b2b43c57f7ad9fa1da7892c7d0920df1
iskgorka <ihprishchepa@gmail.com>
2022-01-03 20:21:15 +0200

    added logic and operations in loop

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,85 @@
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Comparator;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input 3 words: ");
+        System.out.println("Input minimum 3 words: ");
         String inputValue = sc.nextLine();
-        String[] words = inputValue.split("[ ,]+");
-        //System.out.println(Arrays.toString(words));
-        /*
-        for (String word : words) {
-            System.out.println(word);
-        }
-         */
-        System.out.println("Choose the variant: \n" +
-                "1-max length макс длина\n" +
-                "2-start with первые буквы\n" +
-                "3-end with последние буквы\n" +
-                "4-contains часть слова\n" +
+        String[] words = inputValue.split(",");
+        System.out.println("Choose the variant:\n" +
+                "1-max length\n" +
+                "2-start with\n" +
+                "3-end with\n" +
+                "4-contains\n" +
                 "0-exit");
         int variant = sc.nextInt();
-        while (true){
-            if (variant == 1){
-                String longest = Arrays.stream(inputValue.split("[ ,]+")).max(Comparator.comparingInt(String::length)).orElse(null);
-                System.out.println(longest.length());
-                System.out.println("Input another variant");
+        while (variant != 0) {
+            if (variant == 1) {
+                int maxLength = 0;
+                String longestString = null;
+                for (String word : words) {
+                    if (word.length() > maxLength) {
+                        maxLength = word.length();
+                        longestString = word;
+                    }
+                }
+                System.out.println("The longest word is " + longestString);
+                System.out.println("Choose the variant:\n" +
+                        "1-max length\n" +
+                        "2-start with\n" +
+                        "3-end with\n" +
+                        "4-contains\n" +
+                        "0-exit");
+                variant = sc.nextInt();
+
+            } else if (variant == 2) {
+                System.out.println("Input the beginning of the string: ");
+                String startWith = sc.next();
+                for (String word : words) {
+                    if (word.startsWith(startWith)) {
+                        System.out.println("Yes, the '" + word + "' starts with " + startWith);
+                    }
+                }
+                System.out.println("Choose the variant:\n" +
+                        "1-max length\n" +
+                        "2-start with\n" +
+                        "3-end with\n" +
+                        "4-contains\n" +
+                        "0-exit");
+                variant = sc.nextInt();
+
+            } else if (variant == 3) {
+                System.out.println("Input the end of the string: ");
+                String endWith = sc.next();
+                for (String word : words) {
+                    if (word.endsWith(endWith)) {
+                        System.out.println("Yes, the '" + word + "' ends with " + endWith);
+                    }
+                }
+                System.out.println("Choose the variant:\n" +
+                        "1-max length\n" +
+                        "2-start with\n" +
+                        "3-end with\n" +
+                        "4-contains\n" +
+                        "0-exit");
+                variant = sc.nextInt();
+
+            } else if (variant == 4) {
+                System.out.println("Input some text: ");
+                String part = sc.next();
+                for (String word : words) {
+                    if (word.contains(part)) {
+                        System.out.println("Yes, the '" + word + "' contains " + part);
+                    }
+                }
+                System.out.println("Choose the variant:\n" +
+                        "1-max length\n" +
+                        "2-start with\n" +
+                        "3-end with\n" +
+                        "4-contains\n" +
+                        "0-exit");
                 variant = sc.nextInt();
             }
-            if (variant == 2){
-                char first = inputValue.charAt(0);
-                System.out.println(first);
-                System.out.println("Input another variant");
-                variant = sc.nextInt();
-            }
-            if (variant == 3){
-                int last = inputValue.length()-1;
-                char ch = inputValue.charAt(last);
-                System.out.println(ch);
-                System.out.println("Input another variant");
-                variant = sc.nextInt();
-            }
-            if (variant == 4){
-                System.out.println(inputValue.substring(1, inputValue.length()-1));
-                System.out.println("Input another variant");
-                variant = sc.nextInt();
-            /*
-            String contains;
-            System.out.println((contains = inputValue.substring(0, inputValue.lastIndexOf(':'))).substring(inputValue.lastIndexOf('')));
-             */
-            }
-            if (variant == 0){
-                break;
-            }
-            //System.out.println("Choose another variant");
         }
     }
 }
